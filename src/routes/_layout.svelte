a4b2bb2606d60b064d4c8bbafce2fc88a0acd914
GIANCARLO ZEVALLOS LECCA <g10n_22_92@hotmail.com>
2023-04-09 20:47:52 +0100

    Diseño y maquetación del Header

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,12 +1,13 @@
 <script>
 	import Nav from '../components/Nav.svelte';
 	import Main from '../components/Main.svelte';
+    import Header from '../components/Header.svelte';
 
 	export let segment;
 </script>
 
+<Header />
 <Nav {segment}/>
-
 <Main>
 	<slot></slot>
 </Main>
